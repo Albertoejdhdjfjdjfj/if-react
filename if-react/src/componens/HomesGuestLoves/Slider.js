9bb67df2fcd42bc762a461692d343ce35fb541a9
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-11-14 22:25:02 +0300

    I corrected lesson-33

--- if-react/src/componens/HomesGuestLoves/Slider.js
+++ if-react/src/componens/HomesGuestLoves/Slider.js
@@ -5,11 +5,11 @@ function Slider(props) {
    let bodyWidth=window.getComputedStyle(document.querySelector('body')).width.slice(0,-2);
    console.log(bodyWidth);
   return (
-     <div className="slider" style={{width: '85.8vw'}} >
-     <div className="slidesOfSlider" style={{left:0,width:'85vw'}}>
+     <div className="slider">
+     <div className="slidesOfSlider" >
     {props.data.map((item)=>(
-     <div className="slide" style={{width: '296px',marginLeft: '16px'}} key={item.id}>
-        <img style={{width: '296px'}} src={item. imageUrl}/>  
+     <div className="slide" key={item.id}>
+        <img  src={item. imageUrl}/>  
         <p>{item.name}</p>
         <p>{item.city} {item.country}</p>
      </div>
@@ -21,18 +21,17 @@ function Slider(props) {
   );
 
  function SlideImages(){
-  let bodyWidth=window.getComputedStyle(document.querySelector('body')).width.slice(0,-2);
    let sliderLine=document.querySelector('.slidesOfSlider');
    let slider=document.querySelector('.slider');
    let slide=document.querySelector('.slide');
 
-   let sliderWidth=Number(slider.style.width.slice(0,-2))*bodyWidth/100;
-   let slideWidth=Number(slide.style.width.slice(0,-2));
-   let slideMargin=Number(slide.style.marginLeft.slice(0,-2));
-   let sliderLineLeft=sliderLine.style.left;
-
+  
+   let sliderWidth=Number(window.getComputedStyle(slider).width.slice(0,-2));
+   let slideWidth=Number(window.getComputedStyle(slide).width.slice(0,-2));
+   let slideMargin=Number(window.getComputedStyle(slide).marginLeft.slice(0,-2));
+   let sliderLineLeft=window.getComputedStyle(sliderLine).left;
    if(-Number(sliderLineLeft.slice(0,-2))<props.data.length*(slideMargin+slideWidth)-sliderWidth){
-      sliderLine.style.left=Number(sliderLine.style.left.slice(0,-2))-slideWidth-slideMargin+'px';
+      sliderLine.style.left=Number(sliderLineLeft.slice(0,-2))-slideWidth-slideMargin+'px';
    }
    else{
       sliderLine.style.left=0+'vw';
