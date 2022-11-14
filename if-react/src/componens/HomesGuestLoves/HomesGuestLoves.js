dded75318e4fefb1349dd9f13df5aa9ec9059744
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-11-14 03:55:42 +0300

    I corrected lesson 33

---
+++ if-react/src/componens/HomesGuestLoves/HomesGuestLoves.js
@@ -0,0 +1,109 @@
+import Slider from "./Slider";
+const data=[
+  {
+    id: '71ce9eac-e9b9-44f0-a342-9ff0b565f3b7',
+    name: 'Hotel Leopold',
+    city: 'Saint Petersburg',
+    country: 'Russia',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg',
+  },
+  {
+    id: 'aa560608-a879-48a7-80b6-deff2806b250',
+    name: 'Apartment Sunshine',
+    city: 'Santa  Cruz de Tenerife',
+    country: 'Spain',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg',
+  },
+  {
+    id: '1d88fefe-edf1-4cda-844a-babbf29bb2b3',
+    name: 'Villa Kunerad',
+    city: 'Vysokie Tatry',
+    country: 'Slowakia',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg',
+  },
+  {
+    id: 'a2bf824d-edd8-41f0-8b70-244334086ab4',
+    name: 'Hostel Friendship',
+    city: 'Berlin',
+    country: 'Germany',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
+  },
+  {
+    id: '4024535d-a498-4274-b7cb-f01ada962971',
+    name: 'Radisson Blu Hotel',
+    city: 'Kyiv',
+    country: 'Ukraine',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
+  },
+  {
+    id: 'e51e71f6-6baf-4493-b3ae-25dc27cdc238',
+    name: 'Paradise Hotel',
+    city: 'Guadalupe',
+    country: 'Mexico',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
+  },
+  {
+    id: '87d2b966-2431-43f3-8c0d-2c8723474dfc',
+    name: 'Hotel Grindewald',
+    city: 'Interlaken',
+    country: 'Switzerland',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
+  },
+  {
+    id: '190221c6-b18f-4dba-97de-e35f0e14c023',
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
+  },
+  {
+    id: 'e51e71f6-6ba-4493-b3ae-25dc27cdc238',
+    name: 'Paradise Hotel',
+    city: 'Guadalupe',
+    country: 'Mexico',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
+  },
+  {
+    id: '87d2b966-241-43f3-8c0d-2c8723474dfc',
+    name: 'Hotel Grindewald',
+    city: 'Interlaken',
+    country: 'Switzerland',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
+  },
+  {
+    id: '190221c6-b18f-dba-97de-e35f0e14c023',
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
+  },
+]
+
+function HomesGuestloves() {
+  return (
+    <div className="HomesGuestloves">
+     <h2>Homes guests loves</h2>
+     <Slider data={data}/>
+    </div>
+  );
+}
+
+export default HomesGuestloves;
+
+// let bodyWidth=window.getComputedStyle(document.querySelector('body')).width.slice(0,-2);
+//    let sliderLine=document.querySelector('.slidesOfSlider');
+//    let slider=document.querySelector('.slider');
+//    let slide=document.querySelector('.slide');
+
+//    let sliderWidth=Number(slider.style.width.slice(0,-2))*bodyWidth/100;
+//    let slideWidth=Number(slide.style.width.slice(0,-2));
+//    let slideMargin=Number(slide.style.marginLeft.slice(0,-2));
+//    let sliderLineLeft=sliderLine.style.left;
+//   console.log(props.data.length*(slideWidth+slideMargin)-sliderWidth);
+
+//    if(-sliderLineLeft<props.data.length*(slideWidth+slideMargin)-sliderWidth){
+//       sliderLineLeft=Number(sliderLineLeft.slice(0,-2))-slideWidth-slideMargin+'px';
+//    }
+//    else{
+//       sliderLineLeft=0+'px';
+//    }
---
+++ if-react/src/componens/HomesGuestLoves/Slider.js
@@ -0,0 +1,44 @@
+import arrowLeft from '../images/arrow.png';
+import "./HomesGuestLoves.css"
+
+function Slider(props) {
+  return (
+     <div className="slider" style={{width: '85.8vw'}} >
+     <div className="slidesOfSlider" style={{left:0,width:'85vw'}}>
+    {props.data.map((item)=>(
+     <div className="slide" style={{width: '296px',marginLeft: '16px'}} key={item.id}>
+        <img style={{width: '296px'}} src={item. imageUrl}/>  
+        <p>{item.name}</p>
+        <p>{item.city} {item.country}</p>
+     </div>
+    ))
+    }
+   </div>
+   <img src={arrowLeft} onClick={SlideImages} alt='' />
+   </div>
+  );
+
+ function SlideImages(){
+  let bodyWidth=window.getComputedStyle(document.querySelector('body')).width.slice(0,-2);
+   let sliderLine=document.querySelector('.slidesOfSlider');
+   let slider=document.querySelector('.slider');
+   let slide=document.querySelector('.slide');
+
+   let sliderWidth=Number(slider.style.width.slice(0,-2))*bodyWidth/100;
+   let slideWidth=Number(slide.style.width.slice(0,-2));
+   let slideMargin=Number(slide.style.marginLeft.slice(0,-2));
+   let sliderLineLeft=sliderLine.style.left;
+
+   if(-Number(sliderLineLeft.slice(0,-2))<props.data.length*(slideMargin+slideWidth)-sliderWidth){
+      sliderLine.style.left=Number(sliderLine.style.left.slice(0,-2))-slideWidth-slideMargin+'px';
+   }
+   else{
+      sliderLine.style.left=0+'vw';
+   }
+}
+
+
+}
+
+
+export default Slider;
