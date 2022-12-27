005bd23904954ccc5307462195a86000692771f5
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-12-27 21:37:45 +0300

    Icorrected faileds

--- if-react/webpack.config.js
+++ if-react/webpack.config.js
@@ -25,25 +25,7 @@ module.exports={
                     exclude:/node_modules/,
                     use:[
                          {loader:"style-loader"},
-                         {
-                              loader:"css-loader",
-                              options:{
-                                   modules:{
-                                        localIdentName:'[name]__[local]__[hash:base64:5]',
-                                   },
-                                   sourceMap:true
-                              }
-                         },
-                         {
-                              loader:"postcss-loader",
-                              options:{
-                                   postcssOptions:{
-                                        plugins:[
-                                             ['autoprefixer',{},],
-                                        ]
-                                   }
-                              }
-                         }
+                         {loader:"css-loader"}
                     ]
                },
                {
